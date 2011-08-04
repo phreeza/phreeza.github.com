e5c60de3c28b05be612bfad0e641a078a3e18a62
Thomas McColgan <thomas.mccolgan@gmail.com>
2011-08-04 14:56:55 +0200

    remove _pomodoro from ids

--- rispen.js
+++ rispen.js
@@ -1,21 +1,21 @@
 $(function() {
-  $("#timer_pomodoro").createTimer({
+  $("#timer").createTimer({
     time_in_seconds: 25*60,
   tick:function(timer, time_in_seconds, formatted_time)
   {
-    $("#progress_pomodoro").attr("value",$("#timer_pomodoro").data('countdown.duration'));
+    $("#progress").attr("value",$("#timer").data('countdown.duration'));
   }
   ,
   autostart: false});
 
   $("#startstop").click(function() {
-    if ($("#timer_pomodoro").data('countdown.state') == 'running')
+    if ($("#timer").data('countdown.state') == 'running')
   {
-    $("#timer_pomodoro").resetTimer();
+    $("#timer").resetTimer();
   }
     else
   {
-    $("#timer_pomodoro").startTimer($("#timer_pomodoro").data('countdown.settings'));
+    $("#timer").startTimer($("#timer").data('countdown.settings'));
     var newli = document.createElement('li');
     newli.innerHTML = $("#description").val();
 
