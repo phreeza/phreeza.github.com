8304ba280c4735d2e4023f7dbbad4b1ca7c483af
Christopher Grabinski <c.grabinski@gmail.com>
2011-08-13 15:50:16 +0200

    added footer
    I added a footer with a scroll to top function.

--- js/rispen.js
+++ js/rispen.js
@@ -93,3 +93,15 @@ $(function() {
 
   repopTasks($("#chiffre").val())
 });
+
+$(document).ready(function() {
+	$('a[href*=#]').bind("click", function(event) {
+		event.preventDefault();
+		var ziel = $(this).attr("href");
+
+		$('html,body').animate({
+			scrollTop: $(ziel).offset().top
+		}, 500 , function (){location.hash = ziel;});
+});
+return false;
+});
